841c58192d9f0024bf939b86d22b4a1501d2a6d2
jlesko <john.lesko@ge.com>
2019-03-05 00:11:12 -0500

    "Update to conditional_probability_calc"

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from pandas import DataFrame
 
 
 def rps():
@@ -7,11 +6,11 @@ def rps():
     Rock-Paper-Scissors game
     """
     probability_list = pd.DataFrame()
-    USER_PREDICTION = ''
-    while USER_PREDICTION != 'stop':
-        USER_PREDICTION = raw_input('Input rock, paper, scissors: ').lower()
-        probability_list.append(USER_PREDICTION)
-        print probability_list
+    user_prediction = ''
+    while user_prediction != 'stop':
+        user_prediction = raw_input('Input rock, paper, scissors: ').lower()
+        probability_list.append(user_prediction)
+        print conditional_probability_calc(probability_list)
 
 
 def conditional_probability_calc(prediction):
@@ -20,23 +19,26 @@ def conditional_probability_calc(prediction):
     probability of the next choice
     """
 
-    conditional_probability_1 = []
+    conditional_probability = []
+
     if prediction[-1] == 1:
         for i, j in enumerate(prediction):
-            if j == 1:
-                conditional_probability_1.append(prediction[i+1])
-                print i, j, conditional_probability_1
-
-    #     next_choice_prob = pd.DataFrame(prediction, columns=['selection'])
-    #     return next_choice_prob.groupby('selection').size().div(len(next_choice_prob)).idxmax()
-    # elif prediction(-1) == 2:
-    #     return next_choice_prob.groupby('selection').size().div(len(next_choice_prob)).idxmax()
-    # else:
-    #     return next_choice_prob.groupby('selection').size().div(len(next_choice_prob)).idxmax()
-
-
-
-
-if __name__ == '__main__':
-    test = [1, 2, 3, 1, 2, 3, 2, 3, 2, 1, 2, 1, 2, 3, 2, 1]
-    conditional_probability_calc(test)
+            if (j == 1) and (len(prediction) != i+1):
+                conditional_probability.append(prediction[i+1])
+                print i+1, len(prediction), j, conditional_probability
+        next_choice_prob = pd.DataFrame(conditional_probability, columns=['selection'])
+        return next_choice_prob.groupby('selection').size().div(len(next_choice_prob)).idxmax()
+    elif prediction[-1] == 2:
+        for i, j in enumerate(prediction):
+            if (j == 2) and (len(prediction) != i+1):
+                conditional_probability.append(prediction[i+1])
+                print i+1, len(prediction), j, conditional_probability
+        next_choice_prob = pd.DataFrame(conditional_probability, columns=['selection'])
+        return next_choice_prob.groupby('selection').size().div(len(next_choice_prob)).idxmax()
+    else:
+        for i, j in enumerate(prediction):
+            if (j == 3) and (len(prediction) != i+1):
+                conditional_probability.append(prediction[i+1])
+                print i+1, len(prediction), j, conditional_probability
+        next_choice_prob = pd.DataFrame(conditional_probability, columns=['selection'])
+        return next_choice_prob.groupby('selection').size().div(len(next_choice_prob)).idxmax()
